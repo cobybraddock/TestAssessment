2c8e41107147b720d5064b7911c8b4de33d18102
Coby Braddock <CobyBraddock@myopenpolytechnic.ac.nz>
2018-09-19 05:07:09 +0100

    commit 2

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,14 @@
 
 public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi, My Name Is Coby");
+		
+		System.out.println("My favourite colour is red");
+		
+		
+		
+	}
 
 }
